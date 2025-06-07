7adfea335a4e29f1453e2876ac9d9947f109477d
zerihunny <zerisht.444@gmail.com>
2025-06-07 16:16:24 +0300

    there will be an order in which I could be greate one day

---
+++ Item.java
@@ -0,0 +1,14 @@
+public class Item {
+    private String name;
+    private int quantity;
+    public Item (String name, int quantity){
+        this.name = name;
+        this.quantity = quantity;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getQuantitty(){
+        return quantity;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class username {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }

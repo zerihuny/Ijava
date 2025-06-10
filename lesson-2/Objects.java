88c148168413473c229ee7010a32d825df5455ca
zerihunny <zerisht.444@gmail.com>
2025-06-10 14:28:11 +0300

    pr #34

--- lesson-2/Objects.java
+++ lesson-2/Objects.java
@@ -6,6 +6,7 @@ import java.net.URL;
 public class Objects {
     public static void main(String[] args) {
     String name = new String("Binium");
+    String bini = new String ("ad");
     URL adr = new URL("github.com")
     }
 }

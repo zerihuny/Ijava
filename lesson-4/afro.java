b9f105fea4eea24f2dfdde1433f5a725116919d9
zerihunny <zerisht.444@gmail.com>
2025-06-16 09:46:46 +0300

    fix class name casing in afro.java to follow Java naming conventions

--- lesson-4/afro.java
+++ lesson-4/afro.java
@@ -1,5 +1,15 @@
 public class afro {
+  
+    
+
     public static void main(String[] args) {
-        System.out.println("Hello from Afro!");
+        // System.out.println("Hello from Afro!");
+        Object[] boxs = new Object[10];
+        boxs[0] = "Hello";
+        boxs[1] = 123;
+        boxs[2] = 45.67;
+        boxs[3] = true;
+        boxs[4] = new int[]{1, 2, 3};
+
     }
 }

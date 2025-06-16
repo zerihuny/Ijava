970397c45f37fe33f6bd10ef97bbbc6821100944
zerihunny <zerisht.444@gmail.com>
2025-06-16 21:47:11 +0300

    refactor ConstructorExample.java to improve clarity in object creation examples

--- lesson-5/ConstructorExample.java
+++ lesson-5/ConstructorExample.java
@@ -9,14 +9,15 @@ class Dog{
 }
 public class ConstructorExample{
     public static void main(String[] args) {
-        // final int MAX_AGE = 100; // constant value
-        // MAX_AGE = 101; // This line would cause a compilation error because MAX_AGE is final
-        // System.out.println("The maximum age is: " + MAX_AGE);
+
         System.out.println("This is an example of creating connstructor in Java and You can create a Dog object using the constructor.");
 
-        Dog dog1 = new Dog("Buddy", 5);
-        dog1.name = "Max"; // You can change the name
-        dog1.age = 6; // You can change the age
+        // Dog dog1 = new Dog("Buddy", 5);
+        // or it can be created like this
+        Dog dog1;
+        dog1 = new Dog("Bobbie", 4);
+        // dog1.name = "Max"; // You can change the name
+        // dog1.age = 6; // You can change the age
         System.out.println("Dog's name: " + dog1.name);
         System.out.println("Dog's age: " + dog1.age);
         

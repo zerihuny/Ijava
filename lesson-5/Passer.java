26db7c826f47834cec7d38d18f5f21f8d4b344f7
zerihunny <zerisht.444@gmail.com>
2025-06-18 00:01:25 +0300

    rename toUpperCase method to toUpper and update main method to reflect changes

--- lesson-5/Passer.java
+++ lesson-5/Passer.java
@@ -1,5 +1,5 @@
 public class Passer {
-    void toUpperCase(String[] text) {
+    void toUpper(String[] text) {
         for (int i = 0; i < text.length; i++) {
             text[i] = text[i].toUpperCase(); // modifies the original array elements
             text[i] = text[i].replaceAll("[^A-Za-z]", ""); // removes non-alphabetic characters
@@ -17,10 +17,10 @@ public class Passer {
 
     public static void main(String[] args) {
         Passer passer = new Passer();
-        passer.toUpperCase(args); // passing array of strings
+        passer.toUpper(args); // passing array of strings
 
-        for (String arg : args) {
-            System.out.print(arg + " ");
+       for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + " ");
         }
         System.out.println();
     }

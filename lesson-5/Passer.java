69da159390e8043764ee858ecc02365318d00687
zerihunny <zerisht.444@gmail.com>
2025-06-17 23:54:18 +0300

    enhance toUpperCase method to remove non-alphabetic characters from input strings

--- lesson-5/Passer.java
+++ lesson-5/Passer.java
@@ -2,6 +2,8 @@ public class Passer {
     void toUpperCase(String[] text) {
         for (int i = 0; i < text.length; i++) {
             text[i] = text[i].toUpperCase(); // modifies the original array elements
+            text[i] = text[i].replaceAll("[^A-Za-z]", ""); // removes non-alphabetic characters
+            
         }
     }
 

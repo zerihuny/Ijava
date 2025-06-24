8d2044c04381c60757a36ba2f6836fae5cd1fcbd
Zerihuny <zerisht.444@gmail.com>
2025-06-24 11:04:52 +0300

    add Book class with constructors and initial implementation

---
+++ lesson-6/Chain.java
@@ -0,0 +1,18 @@
+class Book {
+    String title;
+    int pages;
+    Book (String title){
+        this(title, 0)
+    }
+    Book (String title, int pages){
+        this.title = title;
+        this.pages = pages;
+}
+
+
+public class Chain {
+    public static void main(String[] args) {
+        
+    }
+    
+}

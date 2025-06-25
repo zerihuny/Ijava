b11e778e66d90f391ce82abc48795961dfd12bb9
Zerihuny <zerisht.444@gmail.com>
2025-06-25 23:28:57 +0300

    implementation in thss.java

---
+++ lesson-6/thss.java
@@ -0,0 +1,43 @@
+public class thss {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero");
+                    scanner.close();
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                scanner.close();
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        scanner.close();
+    }
+}

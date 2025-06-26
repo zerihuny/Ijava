bbe3593a713f552cd2c4d1deda9c6a2419b27cf8
Zerihuny <zerisht.444@gmail.com>
2025-06-26 23:54:05 +0300

    recap #54

--- lesson-6/rec.java
+++ lesson-6/rec.java
@@ -7,3 +7,14 @@ class User {
 
     public int getAge() { return age; }
 }
+public class rec {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setAge(25);
+        System.out.println("User age: " + user.getAge());
+        
+        // Uncommenting the following line will cause a compilation error
+        // because setAge is private.
+        // user.setAge(-5);
+    }
+}

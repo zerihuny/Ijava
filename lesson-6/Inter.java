73ebe3ec389d43e0e2854c52859b99cd4eae7040
Zerihuny <zerisht.444@gmail.com>
2025-07-01 10:31:50 +0300

    Add demonstration of multiple interfaces in Java with Duck class implementation

---
+++ lesson-6/Inter.java
@@ -0,0 +1,30 @@
+// This code demonstrates the use of multiple interfaces in Java.
+// It defines two interfaces, Flyable and Swimmable, and a class Duck that implements both interfaces.
+// The Duck class provides implementations for the methods defined in both interfaces.      
+// The main method creates an instance of Duck and calls its methods to demonstrate functionality.
+// Interfaces allow for a form of multiple inheritance in Java, enabling classes to implement multiple behaviors.
+// Interfaces allow for a flexible design, where classes can be designed to adhere to multiple contracts without being tied to a specific class hierarchy.
+// This is a simple example of how interfaces can be used to define capabilities that can be shared across different classes.
+// This code is a demonstration of using interfaces in Java.
+interface Flyable {
+    void fly();
+}
+interface Swimmable {
+    void swim();
+}
+class Duck implements Flyable, Swimmable {
+    @Override
+    public void fly(){
+        System.out.println("Duck is flying");
+    }
+    public void swim(){
+        System.out.println("Duck is swimming");
+    }
+}
+public class Inter {
+    public static void main(String[] args) {
+        Duck duck = new Duck();
+        duck.fly();
+        duck.swim();
+    }
+}

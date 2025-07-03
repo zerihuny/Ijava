1bb60417b4410e8d3c946c28047607e54f158844
Zerihuny <zerisht.444@gmail.com>
2025-07-03 23:55:27 +0300

    dsfadsf

--- lesson-6/Chain.java
+++ lesson-6/Chain.java
@@ -36,4 +36,5 @@ public class Chain {
 // The code demonstrates constructor chaining in Java, where one constructor calls another to avoid code duplication.   
 // // The code demonstrates constructor chaining in Java, where one constructor calls another to avoid code duplication.
 // The code defines a Book class with a constructor that can be called with one or two parameters.
-// 
+// // The code demonstrates constructor chaining in Java, where one constructor calls another to avoid code duplication.
+// The code demonstrates constructor chaining in Java, where one constructor calls another to avoid code duplication.

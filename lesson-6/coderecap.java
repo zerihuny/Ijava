8debc543ecb959a46a9bcf7e647d52e6c6a24caf
zerihunny <zerisht.444@gmail.com>
2025-07-04 21:41:40 +0300

    pr#54

--- lesson-6/coderecap.java
+++ lesson-6/coderecap.java
@@ -12,6 +12,22 @@ public static int getCount() {
 }
 
 public InstanceCounter() {
+    // Constructor increments the instance count when a new InstanceCounter object is created.
+    // This allows us to keep track of how many instances of this class have been created.
+    // This is useful for debugging or monitoring purposes, as it provides a way to see how many times this class has been instantiated.
+    // The static method addInstance() is called to increment the count of instances.
+    // This is a simple way to implement an instance counter in Java.
+    // The static method addInstance() is called to increment the count of instances.
+    // This is a simple way to implement an instance counter in Java.
+    // The constructor increments the instance count when a new InstanceCounter object is created.
+    // This allows us to keep track of how many instances of this class have been created.
+
+    // This is useful for debugging or monitoring purposes, as it provides a way to see how many times this class has been instantiated.
+    // The static method addInstance() is called to increment the count of instances.
+    // This is a simple way to implement an instance counter in Java.
+    // The static method addInstance() is called to increment the count of instances.
+    // This is a simple way to implement an instance counter in Java.
+    // The constructor increments the instance count when a new InstanceCounter object is created.
     InstanceCounter.addInstance();
 }
 

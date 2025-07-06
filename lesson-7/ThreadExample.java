4966c518446af5d692883f94fa53479aa6ce111c
Zerihuny <zerisht.444@gmail.com>
2025-07-06 23:51:11 +0300

    nbs

--- lesson-7/ThreadExample.java
+++ lesson-7/ThreadExample.java
@@ -12,6 +12,7 @@ public class ThreadExample {
         thread1.start();
 
         // Creating a thread by implementing Runnable interface
+
         Thread thread2 = new Thread(new MyRunnable());
         thread2.start();
     }
@@ -35,3 +36,14 @@ class MyRunnable implements Runnable {
         System.out.println("Thread 2 is running.");
     }
 }
+// This code demonstrates how to create threads in Java using two different approaches:
+// 1. By extending the `Thread` class.  
+// 2. By implementing the `Runnable` interface.
+// The `MyThread` class extends `Thread` and overrides the `run` method to define the thread's behavior.
+// The `MyRunnable` class implements `Runnable` and also overrides the `run` method to define the thread's behavior.
+// The `ThreadExample` class contains the `main` method where instances of both thread types are created and started.
+// The `run` method in each class defines the behavior of the thread when it is started.
+// The `main` method creates instances of both thread types and starts them, demonstrating how to run multiple threads concurrently in Java.
+// The `main` method creates instances of both thread types and starts them, demonstrating how to run multiple threads concurrently in Java.
+// The `run` method in each class defines the behavior of the thread when it is started.
+// The `main` method creates instances of both thread types and starts them, demonstrating how to

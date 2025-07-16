3883f485e03dc8f94895689212e5570fe1fb36db
zerihunny <zerisht.444@gmail.com>
2025-07-16 21:09:57 +0300

    pr #54

---
+++ lesson-7/MyThread.java
@@ -0,0 +1,12 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread is running: " + getName());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start(); // Starts the thread
+    }
+}

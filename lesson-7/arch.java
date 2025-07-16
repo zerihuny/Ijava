dbe1cc84709ed4ddcecef27ed54fdf2f4f5991c5
zerihunny <zerisht.444@gmail.com>
2025-07-16 23:58:07 +0300

    bini

--- lesson-7/arch.java
+++ lesson-7/arch.java
@@ -40,3 +40,4 @@ public class arch {
 }
 ///// This is a simple Java program that demonstrates the creation and execution of threads.
 // It defines a custom thread class, starts two threads, and prints messages from both the main
+// thread and the custom threads. The custom threads print a count from 1 to 5 with a delay.

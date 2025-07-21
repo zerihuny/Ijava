5167d1e091decb6f8d51a7cb5c73d50889f9740d
zerihunny <zerisht.444@gmail.com>
2025-07-21 23:56:36 +0300

    pr@54

--- lesson-7/MyThread.java
+++ lesson-7/MyThread.java
@@ -11,12 +11,6 @@ public class Main {
     }
 }
 
-// This is a basic example of using threads in Java, demonstrating how to create and start a thread.
-// The code is structured to be simple and clear, focusing on the essential aspects of thread creation and execution in Java.       
-// The code is designed to be easy to understand for those new to Java threading concepts.
-// The class MyThread extends Thread and overrides the run method to print a message when the thread is running.
-// The Main class creates an instance of MyThread and starts it, which will execute the run method in a new thread.
-// The output will show the thread's name when it runs.
 // This is a basic example of using threads in Java, demonstrating how to create and start a
 // thread. The code is structured to be simple and clear, focusing on the essential aspects of thread creation and execution in Java.
 // The code is designed to be easy to understand for those new to Java threading concepts.  

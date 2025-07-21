772484e57ae486a6c03dcea04639e1d73a9833bf
zerihunny <zerisht.444@gmail.com>
2025-07-21 23:57:55 +0300

    pr#54

--- lesson-7/MyThread.java
+++ lesson-7/MyThread.java
@@ -15,3 +15,4 @@ public class Main {
 // thread. The code is structured to be simple and clear, focusing on the essential aspects of thread creation and execution in Java.
 // The code is designed to be easy to understand for those new to Java threading concepts.
 // The class MyThread extends Thread and overrides the run method to print a message when the thread
+// is running. The Main class creates an instance of MyThread and starts it, which will execute the run method in a new thread. 

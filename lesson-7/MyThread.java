cf9d70eb0a78f52ee3c0f567cf1c2f056843b962
Zerihun Taressa <34819166+zerihuny@users.noreply.github.com>
2025-07-24 22:56:17 +0300

    pr#54

--- lesson-7/MyThread.java
+++ lesson-7/MyThread.java
@@ -11,6 +11,14 @@ public class Main {
     }
 }
 
+
+
+// This Java code defines a simple thread using the Thread class.
+// The MyThread class extends Thread and overrides the run method to print a message.
+// In the main method, an instance of MyThread is created and started, which will execute
+// the run method in a new thread.
+// The output will show that the thread is running, along with the thread's name.   
+
 // This code demonstrates how to create and start a thread in Java.
 // The MyThread class extends the Thread class and overrides the run method to define the thread's behavior.
 // In the main method, an instance of MyThread is created and started, which will execute the run method in a new thread.

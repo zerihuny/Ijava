cd1736e38897bdc29d37b598fdcfa8caa41da523
Zerihun Taressa <34819166+zerihuny@users.noreply.github.com>
2025-08-01 12:57:52 +0300

    started Go with do for dummies

---
+++ Go/firstGo.go
@@ -0,0 +1,7 @@
+package main 
+import "fmt"
+func main() {
+	fmt.Println("THis is my first Go program")
+	fmt.Println("I am learning Go programming language!")
+	fmt.Println("Go is a statically typed, compiled language designed for simplicity and efficiency.")	
+}
